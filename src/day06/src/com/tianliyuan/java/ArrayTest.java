b3399f59ecfba26e72672c64064374709aa35a02
nanYuan <458653462@qq.com>
2020-09-24 22:51:20 +0800

    change

--- src/day06/src/com/tianliyuan/java/ArrayTest.java
+++ src/day06/src/com/tianliyuan/java/ArrayTest.java
@@ -5,14 +5,15 @@ public class ArrayTest {
 		// 一维数组的声明
 		int[] ids;
 		// 1.1静态初始化(数组的初始化和数组元素的赋值操作，同时进行)；
-		ids = new int[]{1001, 1002, 1003, 1004};
+		ids = new int[] { 1001, 1002, 1003, 1004 };
 		// 1.2 动态初始化（数组）
 		char[] names = new char[5];
+		System.out.println(ids);
 		names[3] = 97;
-		for(int i = 0, len = names.length; i < len; i++) {
+		for (int i = 0, len = names.length; i < len; i++) {
 			System.out.println(names[i]);
 		}
-		
+
 		if (names[0] == 0) {
 			System.out.println("nihao");
 		}
